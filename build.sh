84a5245226d0ab2e0507c7e93810a437ff012179
Joan Medeiros <joanmedeiros2018@gmail.com>
2026-02-08 15:01:07 -0300

    adicionando comando para criação de superusuario

--- build.sh
+++ build.sh
@@ -7,3 +7,7 @@ python manage.py collectstatic --noinput
 python manage.py migrate
 
 python manage.py create_user_groups
+
+if [[$CREATE_SUPERUSER == "true"]]; then
+    python manage.py createsuperuser --noinput 
+fi
